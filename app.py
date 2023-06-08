44f73209046b2c91a165a6633da0bf87dfcc6fb9
Ying Xiong <ying@xiongs.org>
2023-06-07 21:53:26 -0700

    Add a simple chatbot gradio app

--- app.py
+++ app.py
@@ -1,12 +1,51 @@
 import gradio as gr
+import openai
+import time
 
-def greet(name):
-    return "Hello " + name + "!"
+messages = []
+cost = 0
+
+def add_text(history, text):
+    global messages
+    history = history + [(text, '')]
+    messages = messages + [{"role": "user", "content": text}]
+    return history, ""
+
+def generate_response(history, model):
+    global messages, cost
+
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=messages,
+        temperature=0.2)
+
+    response_msg = response.choices[0].message.content
+    cost = cost + (response.usage['total_tokens']) * (0.002 / 1000)
+    messages = messages + [{"role": 'assistant', 'content': response_msg}]
+
+    for char in response_msg:
+        history[-1][1] += char
+        time.sleep(0.05)
+        yield history
+
+def calc_cost():
+    return cost
 
 with gr.Blocks() as demo:
-    name = gr.Textbox(label="Name")
-    output = gr.Textbox(label="Output Box")
-    greet_btn = gr.Button("Greet")
-    greet_btn.click(fn=greet, inputs=name, outputs=output, api_name="greet")
+    radio = gr.Radio(value='gpt-3.5-turbo', choices=['gpt-3.5-turbo','gpt-4'], label='models')
+    chatbot = gr.Chatbot(value=[], elem_id="chatbot").style(height=650)
+    with gr.Row():
+        with gr.Column(scale=0.9):
+            txt = gr.Textbox(
+                show_label=False,
+                placeholder="Enter text and press enter",
+            ).style(container=False)
+        with gr.Column(scale=0.10):
+            cost_view = gr.Textbox(label='usage in $', value=0)
 
-demo.launch()
+    txt.submit(add_text, [chatbot, txt], [chatbot, txt], queue=False).then(
+        generate_response, inputs =[chatbot,radio],outputs = chatbot).then(
+        calc_cost, outputs=cost_view)
+
+
+demo.queue()
